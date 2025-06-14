e0866c284b5855afdc65158ef564fe1831b14716
LimXuanYou0705 <zobby.lim@gmail.com>
2025-06-15 01:23:23 +0800

    Fix GitHub Action to use flutter build web

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      title: 'IoT Water Monitor',
+      home: Scaffold(
+        appBar: AppBar(title: Text("Water Level Monitoring")),
+        body: Center(child: Text("Hello, Firebase!")),
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Firebase Setup Demo"),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Text("Firebase is initialized"),
-        ),
-      )
     );
   }
 }
